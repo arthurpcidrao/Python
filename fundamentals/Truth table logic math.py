e24e43b69eb2c5c8d3c3b8db23e4b786f9584c3a
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-08-04 01:09:24 -0300

    etapa_1/3_concluida

--- fundamentals/Truth table logic math.py
+++ fundamentals/Truth table logic math.py
@@ -2,18 +2,32 @@ def sentense_analysis (sentense):
 
     array = sentense.split(' ')
     i = 0
-    for logica in (array):
+    for logic in (array):
         
-        if (logica == "ou"):
+        if (logic == "ou"):
             array[i] = 'v'
         i = i+1
 
     print(array)
 
-def lexical_analysis (outracoisa):
-    bp = 1
+def lexical_analysis (equation):
+    alphabet = "abcdefghijklmnopqrstuvwxyz ~^→↔()"
+    sum = 0
+
+    for logic in (equation):
+        for letter in (alphabet):
+            if (logic == letter):
+                sum = sum + 1
+
+    if (sum == len(equation)):
+        print("Os símbolos estão corretos")
+    
+    else:
+        print("Os símbolos estão errados")
 
 
-sentense = input("Digite uma frase: ")
+#sentense = input("Digite uma frase: ")
+equation = input("Digite uma equação lógica: ")
 
-sentense_analysis(sentense)
+#sentense_analysis(sentense)
+lexical_analysis(equation)
